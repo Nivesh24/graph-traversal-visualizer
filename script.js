e1b91b69f686ac148cbd78fcedee8d774c6433d4
Student ApnaCollege <nivesh_y@ch.iitr.ac.in>
2026-06-02 21:42:36 +0530

    "feat: implement click-and-drag wall drawing state machine"

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const container=document.getElementById("grid-container");
 
+
+let isDrawing=false;
+
 for(let row=0;row<20;row++){
     for(let col=0;col<50;col++){
         const cell = document.createElement("div");
@@ -8,6 +11,28 @@ for(let row=0;row<20;row++){
         cell.dataset.row = row;
         cell.dataset.col = col;
 
+        cell.addEventListener("mousedown",function(){
+            isDrawing=true;
+            if(cell!==start&& cell!==target){
+                cell.classList.add("wall");
+            }
+        })
+        cell.addEventListener("mouseenter",function(){
+            if(isDrawing==true){ 
+                if(cell!==start&& cell!==target){
+                    cell.classList.add("wall");
+                }
+            }
+        })
+
         container.appendChild(cell);
     }
 }
+document.addEventListener("mouseup", function(){
+
+    isDrawing = false;
+
+});
+
+const start=document.querySelector('[data-row="10"][data-col="10"]');
+const target=document.querySelector('[data-row="10"][data-col="40"]');
