68af4d0422e6569e39796c1bb0ac6bb836677461
Student ApnaCollege <nivesh_y@ch.iitr.ac.in>
2026-06-08 21:37:52 +0530

    feat: finalize Dijkstra algorithm with defensive UI routing

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let abortSignal = false;
+let isRunning = false;
+let drawMode="wall";
 function sleep(ms) {
     return new Promise(function(resolve) {
         setTimeout(function(){
@@ -16,16 +19,19 @@ for(let row=0;row<20;row++){
         cell.dataset.row = row;
         cell.dataset.col = col;
 
-        cell.addEventListener("mousedown",function(){
+        cell.addEventListener("mousedown",function(e){
+            e.preventDefault();
             isDrawing=true;
             if(cell!==start&& cell!==target){
-                cell.classList.add("wall");
+                if(drawMode==="wall") cell.classList.add("wall");
+                else cell.classList.add("weight");
             }
         })
         cell.addEventListener("mouseenter",function(){
-            if(isDrawing==true){ 
+            if(isDrawing){ 
                 if(cell!==start&& cell!==target){
-                    cell.classList.add("wall");
+                    if(drawMode==="wall") cell.classList.add("wall");
+                    else cell.classList.add("weight");
                 }
             }
         })
@@ -46,16 +52,24 @@ start.classList.add("start-node");
 target.classList.add("target-node");
 
 
-    let startRow=parseInt(start.dataset.row);
-    let startCol=parseInt(start.dataset.col);
+let startRow=parseInt(start.dataset.row);
+let startCol=parseInt(start.dataset.col);
 
-    let targetRow = parseInt(target.dataset.row);
-    let targetCol = parseInt(target.dataset.col);
+let targetRow = parseInt(target.dataset.row);
+let targetCol = parseInt(target.dataset.col);
 
 
 
-async function executeBFS(){
+async function startAlgorithm(){
+    if(isRunning) return ;
+    isRunning =true;
 
+    abortSignal=false;
+    clearPath();
+    document.getElementById("start-btn").disabled=true;
+    document.getElementById("clear-path-btn").disabled=true;
+    document.getElementById("clear-board-btn").disabled=true;
+    abortSignal=false;
     let visited=[];
     for(let i =0;i<20;i++){
         visited.push(Array(50).fill(false));
@@ -74,8 +88,17 @@ async function executeBFS(){
     let dr=[0,0,-1,1];
     let dc=[-1,1,0,0];
     while(queue.length>0){
-        let x=queue[0];
-        queue.shift();
+        if(abortSignal) return;
+
+        let currentAlgo=document.getElementById("algo-select").value;
+        let x;
+        if(currentAlgo==="bfs") {
+            x=queue.shift(); 
+        } 
+        else{
+            x=queue.pop();  
+        }
+
         let i=x[0];
         let j=x[1];
 
@@ -86,7 +109,7 @@ async function executeBFS(){
         }
 
         await sleep(10);
-
+        if (abortSignal) return;
         for(let k=0;k<4;k++){
             let nr=i+dr[k];
             let nc=j+dc[k];
@@ -99,13 +122,106 @@ async function executeBFS(){
 
                     if(nr===targetRow && nc===targetCol){
                         await drawPath(parent);
+                        isRunning=false;
+                        document.getElementById("start-btn").disabled=false;
+                        document.getElementById("clear-path-btn").disabled=false;
+                        document.getElementById("clear-board-btn").disabled=false;
                         return;
                     }
                 }
             }
         }
     }
+    document.getElementById("start-btn").disabled=false;
+    document.getElementById("clear-path-btn").disabled=false;
+    document.getElementById("clear-board-btn").disabled=false;
+    isRunning=false;
 }
+
+
+async function executeDijkstras(){
+    if(isRunning) return;
+    isRunning=true;
+    abortSignal=false;
+    clearPath();
+    document.getElementById("start-btn").disabled=true;
+    document.getElementById("clear-path-btn").disabled=true;
+    document.getElementById("clear-board-btn").disabled=true;
+    abortSignal=false;
+
+    let distance=[];
+    for(let i =0;i<20;i++){
+        distance.push(Array(50).fill(Infinity));
+    }
+
+    let parent=[];
+    for(let i=0;i<20;i++){
+        parent.push(Array(50).fill(null));
+    }
+
+    parent[startRow][startCol]=null;
+    let queue=[];
+    queue.push([startRow,startCol,0]);
+    distance[startRow][startCol]=0;
+
+    let dr=[0,0,-1,1];
+    let dc=[-1,1,0,0];
+    
+    while(queue.length>0){
+        if(abortSignal) return;
+        queue.sort((a,b)=>a[2]-b[2]);
+
+        let x=queue.shift();
+        let i=x[0];
+        let j=x[1];
+        let dist=x[2];
+
+        if(i===targetRow && j===targetCol){
+            await drawPath(parent);
+            document.getElementById("start-btn").disabled = false;
+            document.getElementById("clear-path-btn").disabled = false;
+            document.getElementById("clear-board-btn").disabled = false;
+            
+            isRunning=false;
+            return;
+        }
+
+        let currentCell=document.querySelector(`[data-row="${i}"][data-col="${j}"]`);
+
+        if (currentCell && !currentCell.classList.contains("start-node") && !currentCell.classList.contains("target-node")) {
+            currentCell.classList.add("visited");
+        }
+
+        await sleep(10);
+        if(abortSignal) return;
+        for(let k=0;k<4;k++){
+            let nr=i+dr[k];
+            let nc=j+dc[k];
+            if(nr>=0&&nr<20&&nc>=0&&nc<50){
+                let neigh=document.querySelector(`[data-row="${nr}"][data-col="${nc}"]`)
+                let weight = 1; 
+                if(neigh.classList.contains("weight")) {
+                    weight=5; 
+                }
+                if(!neigh.classList.contains("wall")){
+                    if(distance[nr][nc]>dist+weight){
+                        distance[nr][nc]=dist+weight
+                        parent[nr][nc]=[i,j];
+                        queue.push([nr,nc,distance[nr][nc]]);
+                    }
+
+                    
+                }
+            }
+        }
+        
+    }
+    document.getElementById("start-btn").disabled=false;
+    document.getElementById("clear-path-btn").disabled=false;
+    document.getElementById("clear-board-btn").disabled=false;
+    isRunning=false;
+}
+
 
 
 async function drawPath(parent){
@@ -126,6 +242,8 @@ async function drawPath(parent){
 
 function clearPath(){
     let cells=document.querySelectorAll(".cell");
+    abortSignal=true;
+    isRunning =false;
     cells.forEach(cell => {
         cell.classList.remove("visited","path");
     });
@@ -133,11 +251,53 @@ function clearPath(){
 
 function clearBoard(){
     let cells=document.querySelectorAll(".cell");
+    abortSignal=true;
+    isRunning=false;
     cells.forEach(function(cell){
-        cell.classList.remove("visited","path","wall")
+        cell.classList.remove("visited","path","wall","weight")
     })
 }
 
-document.getElementById("start-btn").addEventListener("click",executeBFS);
+document.getElementById("start-btn").addEventListener("click",function(){
+    let currentAlgo =document.getElementById("algo-select").value;
+    if(currentAlgo==="dijkstra") {
+        executeDijkstras();
+    } 
+    else{
+        startAlgorithm();  
+    }
+});
 document.getElementById("clear-path-btn").addEventListener("click", clearPath);
 document.getElementById("clear-board-btn").addEventListener("click", clearBoard);
+
+
+let algoSelect = document.getElementById("algo-select");
+let startBtn = document.getElementById("start-btn");
+
+algoSelect.addEventListener("change", function(){
+    startBtn.textContent = "Start " + algoSelect.value.toUpperCase();
+    
+    let mudBtn = document.getElementById("mud-btn");
+    
+    if(algoSelect.value==="dijkstra") {
+        mudBtn.disabled=false; 
+    } 
+    else {
+        mudBtn.disabled = true;  
+        drawMode = "wall"; 
+    }
+});
+if (document.getElementById("algo-select").value !== "dijkstra") {
+    document.getElementById("mud-btn").disabled = true;
+}
+
+
+document.getElementById("wall-btn").addEventListener("click",()=>{
+    drawMode = "wall";
+});
+
+document.getElementById("mud-btn").addEventListener("click",()=>{
+    drawMode = "weight";
+});
+
+
